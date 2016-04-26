9bfe11545fd53d2bcdaffd4c79eb77e09ad98535
Alexander Maksimovich <mak4alex@gmail.com>
2016-04-26 13:18:03 +0300

    Add root path

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
