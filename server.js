170ac665a7ddddaaa8462ad43c49a4911714e94d
Alexander Maksimovich <mak4alex@gmail.com>
2016-04-26 13:58:39 +0300

    Remove * path

--- server.js
+++ server.js
@@ -28,10 +28,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
