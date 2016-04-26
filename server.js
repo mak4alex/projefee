ac6d8b8758d4cdad7118a88fca4955daa788b4ba
Alexander Maksimovich <mak4alex@gmail.com>
2016-04-26 13:39:52 +0300

    Add production load

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 /* eslint no-console: 0 */
 const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const config = require('./webpack.config');
 const port = process.env.PORT || 3000;
-
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath,
-  stats: {
-    colors: true,
-  },
-}));
 
-app.use(require('webpack-hot-middleware')(compiler));
+if (process.env.NODE_ENV === 'production') {
+  const publicPath = express.static(path.join(__dirname, '../public'));
+  app.use('/public', publicPath);
+} else {
+  const webpack = require('webpack');
+  const config = require('./webpack.config');
+  const compiler = webpack(config);
+
+  app.use(require('webpack-dev-middleware')(compiler, {
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+    },
+  }));
+
+  app.use(require('webpack-hot-middleware')(compiler));
+}
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
